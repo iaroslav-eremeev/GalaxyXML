394db5c5eb028cd386abe43dfc4eb2333a57fc7f
iaroslav.eremeev <eremeev.pt@gmail.com>
2022-11-30 12:05:55 +0200

    Add comments in XmlMethods and Generator classes

--- src/main/java/iaroslav/eremeev/util/Generator.java
+++ src/main/java/iaroslav/eremeev/util/Generator.java
@@ -7,28 +7,48 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Generator {
+
+    /**
+     * Random number generator - for numbers in planets' and galaxies' names
+     * @param a - from
+     * @param b - to, not included
+     * @return int number
+     */
     public static int generateNumber(int a, int b){
         Random random = new Random();
         return random.nextInt(b) + a;
     }
 
+    /**
+     * Method for generating types of planets
+     * @return String - type of planet (one of class Planet variables)
+     */
     public static String generateType(){
         Random random = new Random();
         String[] types = {"Desert", "Rocky", "Oceanic", "Volcano", "Forestland"};
         return types[random.nextInt(types.length)];
     }
 
+    /**
+     * Method for generating Planet objects using generateType() and generateNumber()
+     * @return Planet object
+     */
     public static Planet generatePlanet() {
         Planet planet = new Planet();
         planet.setType(generateType());
-        planet.setName("P" + generateNumber(10, 100));
+        planet.setName("P" + generateNumber(10, 90));
         return planet;
     }
 
+    /**
+     * Method for generating a Galaxy object with name G**, where ** is a 2-digit int
+     * Galaxy object has from 1 to 20 planets
+     * @return Galaxy object
+     */
     public static Galaxy generateGalaxy() {
         Galaxy galaxy = new Galaxy();
-        galaxy.setName("G" + generateNumber(10, 100));
-        int numberOfPlanets = generateNumber(1, 21);
+        galaxy.setName("G" + generateNumber(10, 90));
+        int numberOfPlanets = generateNumber(1, 20);
         for (int i = 0; i < numberOfPlanets; i++) {
             galaxy.addPlanet(generatePlanet());
         }
@@ -37,7 +57,7 @@ public class Generator {
 
     public static ArrayList<Galaxy> generateGalaxies(){
         ArrayList<Galaxy> galaxies = new ArrayList<>();
-        int numberOfGalaxies = generateNumber(1, 11);
+        int numberOfGalaxies = generateNumber(1, 10);
         for (int i = 0; i < numberOfGalaxies; i++) {
             galaxies.add(generateGalaxy());
         }
--- src/main/java/iaroslav/eremeev/util/XmlMethods.java
+++ src/main/java/iaroslav/eremeev/util/XmlMethods.java
@@ -19,17 +19,33 @@ import java.io.IOException;
 
 public class XmlMethods {
 
+    /**
+     * Method for parsing XML documents
+     * @param fileName - name of the XML document
+     * @return Document
+     */
     public static Document parseXML(String fileName) throws ParserConfigurationException, IOException, SAXException {
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
         documentBuilderFactory.setIgnoringElementContentWhitespace(true);
         DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
         return documentBuilder.parse(new File(fileName));
     }
+
+    /**
+     * Method for initializing new Document objects
+     * @return Document
+     */
     public static Document newDoc() throws ParserConfigurationException {
         DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
         return docBuilder.newDocument();
     }
+
+    /**
+     * Method for writing XML documents from Document object, uses .xslt file to avoid blank lines
+     * @param doc - Document object
+     * @param fileName - name of the XML file to write to
+     */
     public static void writeToXml(Document doc, String fileName) {
         try (FileOutputStream output =
                      new FileOutputStream(fileName)) {
