28fb8e089e64e2c75ef54d59fd4e59f441ea39a7
iaroslav.eremeev <eremeev.pt@gmail.com>
2022-11-30 17:18:14 +0200

    Add comments in Universe class

--- src/main/java/iaroslav/eremeev/model/Universe.java
+++ src/main/java/iaroslav/eremeev/model/Universe.java
@@ -20,10 +20,19 @@ public class Universe {
     public Universe() {
     }
 
+    /**
+     * Method for adding galaxies in the universe
+     * @param galaxy - Galaxy object
+     */
     public void addGalaxy(Galaxy galaxy) {
         this.galaxies.add(galaxy);
     }
 
+    /**
+     * Method for finding planets in the universe
+     * @param planet - Planet object to find
+     * @return Array of integer indexes of respective galaxies and planets
+     */
     public int[] findPlanet(Planet planet) {
         for (Galaxy galaxy : this.galaxies) {
             if (galaxy.findPlanet(planet) > -1)
@@ -32,6 +41,11 @@ public class Universe {
         return null;
     }
 
+    /**
+     * Method for finding planets by their name
+     * @param planetName - name of Planet object
+     * @return Planet object if found
+     */
     public Planet findPlanet(String planetName) {
         for (Galaxy galaxy : this.galaxies) {
             return galaxy.findPlanet(planetName);
@@ -39,10 +53,20 @@ public class Universe {
         return null;
     }
 
+    /**
+     * Method for finding galaxies in the universe
+     * @param galaxy - Galaxy object
+     * @return - integer index of galaxy if found
+     */
     public int findGalaxy(Galaxy galaxy) {
         return this.galaxies.indexOf(galaxy);
     }
 
+    /**
+     * Method for finding galaxy by its name
+     * @param galaxyName - name of Galaxy object
+     * @return - Galaxy object if found by name
+     */
     public Galaxy findGalaxy(String galaxyName) {
         for (Galaxy galaxy : this.galaxies) {
             if (galaxy.getName().equals(galaxyName)) return galaxy;
@@ -50,6 +74,11 @@ public class Universe {
         return null;
     }
 
+    /**
+     * Behavior method - every 30 seconds generates from 1 to 10 (number may be changed)
+     * new galaxies with random number of planets as specified in Generator.generateGalaxies()
+     * When updated, prints the Array of galaxies generated
+     */
     public void behavior() {
         while (true) {
             ArrayList<Galaxy> galaxies = Generator
@@ -63,6 +92,11 @@ public class Universe {
         }
     }
 
+    /**
+     * Parse Universe object from an XML document
+     * @param fileName - name of the XML document
+     * @return - Universe object
+     */
     public Universe fromXML(String fileName) throws ParserConfigurationException, IOException, SAXException {
         Document doc = XmlMethods.parseXML(fileName);
         Universe universe = new Universe();
@@ -77,12 +111,20 @@ public class Universe {
         return universe;
     }
 
+    /**
+     * Write a Universe object to XML document
+     * @param fileName - name of the XML document to write to
+     */
     public void toXML(String fileName) throws ParserConfigurationException {
         Document doc = XmlMethods.newDoc();
         toXmlElement(doc);
         XmlMethods.writeToXml(doc, fileName);
     }
 
+    /**
+     * Transform a Universe object into XML Element
+     * @param doc - XML document
+     */
     public void toXmlElement(Document doc){
         Element universe = doc.createElement("universe");
         doc.appendChild(universe);
@@ -91,6 +133,12 @@ public class Universe {
         for (Galaxy galaxy : this.galaxies) galaxy.toXmlElement(doc, galaxies);
     }
 
+    /**
+     * Group planets with the same names inside tags <EqualGroup></> inside the tag <planets></>
+     * with tag attributes: number - number of each EqualGroup in the <planets></> tag,
+     * name - name of the group formed with the name of the planet and String "_group"
+     * @param fileName - name of the XML file, that must be transformed
+     */
     public void groupPlanetsInXmlFile(String fileName) throws ParserConfigurationException, IOException, SAXException {
         Document doc = XmlMethods.parseXML(fileName);
         NodeList planetsAllGalaxies = doc.getElementsByTagName("planets");
